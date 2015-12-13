647377371047f95985f4e78d07e509738efcfca3
Ankit Laddha <ankit1991laddha@gmail.com>
2015-12-13 18:55:51 -0500

    Experiment 1 changes

--- python/test_sim_expt1.py
+++ python/test_sim_expt1.py
@@ -156,10 +156,12 @@ def change_prev (X,Y,prev=0.05):
 	return X[:,prev_idxs], Y[prev_idxs]
 	
 
-def return_average_positive_neighbors (X, Y, k, use_for=True):
+def return_average_positive_neighbors (X, Y, k, use_for=True, seed=0):
 	Y = np.asarray(Y)
-
-	pos_inds = Y.nonzero()[0]
+	
+	nr.seed(seed)
+	n_sample_pos = 200;
+	pos_inds = Y.nonzero()[0][nr.choice(len(Y.nonzero()[0]),n_sample_pos,replace=False)]
 	npos = len(pos_inds)
 
 	if use_for:
@@ -183,7 +185,6 @@ def return_average_positive_neighbors (X, Y, k, use_for=True):
 		MsimY = Y[MsimInds]
 
 		return MsimY.sum(axis=None)/(npos*k)
-
 
 def test_covtype (seed=0):
 	nr.seed(seed)
@@ -220,6 +221,113 @@ def test_covtype (seed=0):
 
 	cl = 4
 	Y = (Y==cl)
+
+	n_pos = 5
+	n_neg = 100
+
+	n_samples_pos = np.min([n_pos,len(Y.nonzero()[0])])
+	n_samples_neg = np.min([n_neg,len((Y == 0).nonzero()[0])])
+
+	W0 = np.eye(d)
+	print 'Loaded the data'
+
+	init_pt_pos = Y.nonzero()[0][nr.choice(len(Y.nonzero()[0]),n_samples_pos,replace=False)]
+	print 'Sampled the positive data'
+
+	init_pt_neg = (Y == 0).nonzero()[0][nr.choice(len((Y == 0).nonzero()[0]),n_samples_neg,replace=False)]
+	print 'Sampled Negative Data'
+
+	idxs = np.concatenate((init_pt_pos, init_pt_neg))
+	X_sampled = X[:,idxs]
+	Y_sampled = Y[idxs]
+
+	W0 = np.eye(d)
+
+	slprms = SL.SPSDParameters(alpha=sl_alpha, C1=sl_C1, C2=sl_C2, gamma=sl_gamma, margin=sl_margin, 
+		epochs=sl_epochs, npairs_per_epoch=sl_npairs_per_epoch, nneg_per_pair=sl_nneg_per_pair, batch_size=sl_batch_size)
+	# Now learn the similarity using the sampled data
+	sl = SL.SPSD()
+	sl.initialize(X_sampled,Y_sampled,W0,slprms)
+	sl.runSPSD()
+	W = sl.getW()
+	sqrtW = sl.getSqrtW()
+
+	X1 = ss.csr_matrix(sqrtW).dot(X)
+	knn = 100
+
+	# Run Active Search
+	prms = ASI.Parameters(pi=pi,sparse=sparse, verbose=True, eta=eta)
+
+	kAS = ASI.kernelAS (prms)
+	aAS = AAS.adaptiveKernelAS(W0, T, prms, slprms)
+
+	init_pt = Y.nonzero()[0][nr.choice(len(Y.nonzero()[0]),2,replace=False)]
+
+	kAS.initialize(X, init_labels={p:1 for p in init_pt})
+	aAS.initialize(X1, init_labels={p:1 for p in init_pt})
+
+	hits1 = [2]
+	hits2 = [2]
+
+	for i in xrange(K):
+
+		idx1 = kAS.getNextMessage()
+		idx2 = aAS.getNextMessage()
+
+		kAS.setLabelCurrent(Y[idx1])
+		aAS.setLabelCurrent(Y[idx2])
+		print('')
+
+		hits1.append(hits1[-1]+Y[idx1])
+		hits2.append(hits2[-1]+Y[idx2])
+	
+	# Calculate KNN Accuracy
+	knn_avg_native = return_average_positive_neighbors(X, Y, knn, seed)
+	knn_avg_learned = return_average_positive_neighbors(X1, Y, knn, seed)
+
+	save_results = {'kAS': hits1, 'aAS':hits2, 'knn_native':knn_avg_native, 'knn_learned':knn_learned}
+	fname = 'covertype/expt1_seed_%d_npos_%d_nneg_%d.cpk'%(seed, n_samples_pos, n_samples_neg)
+	fname = osp.join(results_dir, fname)
+
+	with open(fname, 'w') as fh: pick.dump(save_results, fh)
+
+
+def test_higgs (seed=0):
+	nr.seed(seed)
+
+	verbose = True
+	sparse = True
+	pi = 0.5
+	eta = 0.7
+	K = 999
+	T = 1000
+
+	sl_alpha = 0.01
+	sl_C1 = 0.0
+	sl_C2 = 1.0
+	sl_gamma = 0.01
+	sl_margin = 0.01
+	sl_sampleR = 5000
+	sl_epochs = 30
+	sl_npairs_per_epoch = 30000
+	sl_nneg_per_pair = 1
+	sl_batch_size = 1000
+	
+	# Stratified sampling
+	strat_frac = 0.1
+	t1 = time.time()
+	X,Y,classes = load_higgs(sparse=sparse)
+	print ('Time taken to load %.2fs'%(time.time()-t1))
+	if strat_frac < 1.0:
+		X, Y = stratified_sample(X, Y, classes, strat_frac=strat_frac)
+
+	# Changing prevalence of +
+	prev = 0.025
+	X,Y = change_prev (X,Y,prev=prev)
+	d,n = X.shape
+
+	X_norms = np.sqrt(((X.multiply(X)).sum(axis=0))).A.squeeze()
+	X = X.dot(ss.spdiags([1/X_norms],[0],n,n)) # Normalization
 
 	n_pos = 500
 	n_neg = 10000
@@ -281,118 +389,11 @@ def test_covtype (seed=0):
 		hits2.append(hits2[-1]+Y[idx2])
 	
 	# Calculate KNN Accuracy
-	knn_avg_native = return_average_positive_neighbors(X, Y, knn)
-	knn_avg_learned = return_average_positive_neighbors(X1, Y, knn)
+	knn_avg_native = return_average_positive_neighbors(X, Y, knn, seed)
+	knn_avg_learned = return_average_positive_neighbors(X1, Y, knn, seed)
 
 	save_results = {'kAS': hits1, 'aAS':hits2, 'knn_native':knn_avg_native, 'knn_learned':knn_learned}
-	fname = 'covertype/expt1_seed_%d_npos_%d_nneg_%d.cpk'%(seed, n_samples_pos, n_samples_neg)
-	fname = osp.join(results_dir, fname)
-
-	with open(fname, 'w') as fh: pick.dump(save_results, fh)
-
-
-def test_higgs (seed=0):
-	nr.seed(seed)
-
-	verbose = True
-	sparse = True
-	pi = 0.5
-	eta = 0.7
-	K = 999
-	T = 1000
-
-	sl_alpha = 0.01
-	sl_C1 = 0.0
-	sl_C2 = 1.0
-	sl_gamma = 0.01
-	sl_margin = 0.01
-	sl_sampleR = 5000
-	sl_epochs = 30
-	sl_npairs_per_epoch = 30000
-	sl_nneg_per_pair = 1
-	sl_batch_size = 1000
-	
-	# Stratified sampling
-	strat_frac = 1.0
-	t1 = time.time()
-	X,Y,classes = load_higgs(sparse=sparse)
-	print ('Time taken to load %.2fs'%(time.time()-t1))
-	if strat_frac < 1.0:
-		X, Y = stratified_sample(X, Y, classes, strat_frac=strat_frac)
-
-	# Changing prevalence of +
-	prev = 0.025
-	X,Y = change_prev (X,Y,prev=prev)
-	d,n = X.shape
-
-	X_norms = np.sqrt(((X.multiply(X)).sum(axis=0))).A.squeeze()
-	X = X.dot(ss.spdiags([1/X_norms],[0],n,n)) # Normalization
-
-	n_pos = 50
-	n_neg = 1000
-
-	n_samples_pos = np.min([n_pos,len(Y.nonzero()[0])])
-	n_samples_neg = np.min([n_neg,len((Y == 0).nonzero()[0])])
-
-	W0 = np.eye(d)
-	print 'Loaded the data'
-
-	init_pt_pos = Y.nonzero()[0][nr.choice(len(Y.nonzero()[0]),n_samples_pos,replace=False)]
-	print 'Sampled the positive data'
-
-	init_pt_neg = (Y == 0).nonzero()[0][nr.choice(len((Y == 0).nonzero()[0]),n_samples_neg,replace=False)]
-	print 'Sampled Negative Data'
-
-	idxs = np.concatenate((init_pt_pos, init_pt_neg))
-	X_sampled = X[:,idxs]
-	Y_sampled = Y[idxs]
-
-	W0 = np.eye(d)
-
-	slprms = SL.SPSDParameters(alpha=sl_alpha, C1=sl_C1, C2=sl_C2, gamma=sl_gamma, margin=sl_margin, 
-		epochs=sl_epochs, npairs_per_epoch=sl_npairs_per_epoch, nneg_per_pair=sl_nneg_per_pair, batch_size=sl_batch_size)
-	# Now learn the similarity using the sampled data
-	sl = SL.SPSD()
-	sl.initialize(X_sampled,Y_sampled,W0,slprms)
-	sl.runSPSD()
-	W = sl.getW()
-	sqrtW = sl.getSqrtW()
-
-	X1 = ss.csr_matrix(sqrtW).dot(X)
-	knn = 100
-
-	# Run Active Search
-	prms = ASI.Parameters(pi=pi,sparse=sparse, verbose=True, eta=eta)
-
-	kAS = ASI.kernelAS (prms)
-	aAS = AAS.adaptiveKernelAS(W0, T, prms, slprms)
-
-	init_pt = Y.nonzero()[0][nr.choice(len(Y.nonzero()[0]),2,replace=False)]
-
-	kAS.initialize(X, init_labels={p:1 for p in init_pt})
-	aAS.initialize(X1, init_labels={p:1 for p in init_pt})
-
-	hits1 = [2]
-	hits2 = [2]
-
-	for i in xrange(K):
-
-		idx1 = kAS.getNextMessage()
-		idx2 = aAS.getNextMessage()
-
-		kAS.setLabelCurrent(Y[idx1])
-		aAS.setLabelCurrent(Y[idx2])
-		print('')
-
-		hits1.append(hits1[-1]+Y[idx1])
-		hits2.append(hits2[-1]+Y[idx2])
-	
-	# Calculate KNN Accuracy
-	knn_avg_native = return_average_positive_neighbors(X, Y, knn)
-	knn_avg_learned = return_average_positive_neighbors(X1, Y, knn)
-
-	save_results = {'kAS': hits1, 'aAS':hits2, 'knn_native':knn_avg_native, 'knn_learned':knn_learned}
-	fname = 'covertype/expt1_seed_%d_npos_%d_nneg_%d.cpk'%(seed, n_samples_pos, n_samples_neg)
+	fname = 'higgs/expt1_seed_%d_npos_%d_nneg_%d.cpk'%(seed, n_samples_pos, n_samples_neg)
 	fname = osp.join(results_dir, fname)
 
 	with open(fname, 'w') as fh: pick.dump(save_results, fh)
